6db148c5f7f59d5f0c7388f0f3469e1559f9c144
Székely Mapper <pasztorg05@gmail.com>
2021-04-13 08:54:10 +0200

    navbar-v01

--- js/index.js
+++ js/index.js
@@ -1,38 +1,36 @@
 $(window).scroll(function() {
 	var $height = $(window).scrollTop();
+	$('#head').removeClass('active');
   if($height > 90) {
 		$('#head').addClass('active');
-	} else {
-		$('#head').removeClass('active');
 	}
 });
 
 
 $(window).scroll(function() {
 	var $height = $(window).scrollTop();
+	$('#middle').removeClass('active');
   if($height > 950) {
 		$('#middle').addClass('active');
-	} else {
-		$('#middle').removeClass('active');
 	}
 });
 
 $(window).scroll(function() {
 	var $height = $(window).scrollTop();
+	$('#leftside').removeClass('active');
+
   if($height >= 1200) {
 		$('#leftside').addClass('active');
-	} else {
-		$('#leftside').removeClass('active');
 	}
 });
 
 $(window).scroll(function() {
 	var $height = $(window).scrollTop();
+	$('#rightside').removeClass('active');
+
   if($height > 120) {
 		$('#rightside').addClass('active');
-	} else {
-		$('#rightside').removeClass('active');
-	}
+  }
 });
  
 
